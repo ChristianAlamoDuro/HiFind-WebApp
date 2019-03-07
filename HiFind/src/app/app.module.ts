1be5f44e7df7499172d95f4204aabc7ce43d3201
christam13 <christam13@iMac-de-Jarvis.local>
2019-03-07 19:24:54 +0100

    Christian: Descargado y configurado font-Awesome, creado el componente navbar,configurado sus estilos

--- HiFind/src/app/app.module.ts
+++ HiFind/src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { SideNavBarComponent } from './components/side-nav-bar/side-nav-bar.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     SideNavBarComponent,
-    BannerComponent
+    BannerComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
