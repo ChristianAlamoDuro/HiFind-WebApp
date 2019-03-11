b39398bc25d50d1035612e9b8b305e7e9359f3bf
unknown <alvarezperez.da@gmail.com>
2019-03-12 00:54:01 +0100

    Daniel: añadidos comentarios en componente more-info

--- HiFind/src/app/components/more-info/more-info.component.ts
+++ HiFind/src/app/components/more-info/more-info.component.ts
@@ -12,9 +12,11 @@ export class MoreInfoComponent implements OnInit {
   public movieTitle: string;
   public movieInfo: any;
   constructor(
+    // Inicializamos en el constructor una variable de tipo activatedRouted
     public activatedRoute: ActivatedRoute,
     public publicApiMovies: PublicMovieApiService,
   ) {
+    // Sacamos la variable que le hemos pasado por parametros
     this.activatedRoute.params.subscribe(
       results => {
         this.movieTitle = results.title;
@@ -26,9 +28,12 @@ export class MoreInfoComponent implements OnInit {
 
   ngOnInit() {
   }
+  // Función a la que se le pasa por parametro el título de la pélicula y hace la llamada a la api llamando a la funcion
+  // getmovie para obtener la información de esa pelicula
   moreInfo(movieTitle: string) {
     this.publicApiMovies.getMovie(movieTitle).subscribe(
       results => {
+        // guardamos en movieInfo lo que nos devuelve la api
         this.movieInfo = results;
       }
     );
