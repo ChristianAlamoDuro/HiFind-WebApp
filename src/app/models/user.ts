1be20faaea4bf8f6062277bccf0a7812bdacbadf
ChristianAlamoDuro <christian.alamoduro@outlook.es>
2019-05-12 22:22:39 +0200

    chore: added new model

---
+++ src/app/models/user.ts
@@ -0,0 +1,11 @@
+export class User{
+    constructor(
+        public id: number,
+        public username: string,
+        public email: string,
+        public role: string,
+        public passwotd: string
+     ) {
+
+    }
+}
