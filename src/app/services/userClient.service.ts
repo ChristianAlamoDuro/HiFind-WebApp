3138d76c8c6fbf36f90ad2e6e6a9c387aa26126c
ChristianAlamoDuro <christian.alamoduro@outlook.es>
2019-05-15 00:19:34 +0200

    chore: added login funtionaly

--- src/app/services/userClient.service.ts
+++ src/app/services/userClient.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
@@ -20,4 +21,15 @@ export class UserClientService {
     return this.http.post(this.url + 'users/register', params, {headers});
   }
 
+  signUp(user, getToken = null): Observable<any> {
+    if (getToken != null) {
+      user.getToken = 'true';
+    }
+
+    const json = JSON.stringify(user);
+    const params = 'json=' + json;
+    const headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
+
+    return this.http.post(this.url + 'users/login', params, {headers});
+  }
 }
