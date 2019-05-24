0090610985fa0236859c978cad09658e00aa712f
ChristianAlamoDuro <christian.alamoduro@outlook.es>
2019-05-24 21:49:03 +0200

    chore: dinamic side navbar

--- src/app/modules/side-navbar/side-navbar.component.ts
+++ src/app/modules/side-navbar/side-navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataAplicationService } from '@services/data-aplication/data-aplication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-side-navbar',
@@ -8,15 +9,46 @@ import { DataAplicationService } from '@services/data-aplication/data-aplication
 })
 export class SideNavBarComponent implements OnInit {
 
-  public dataAplication;
+  public dataAplication: any;
+  public movies = false;
+  public games = false;
+
   constructor(
-    public dataService: DataAplicationService
-  ) { }
+    private dataService: DataAplicationService,
+    private router: Router
+  ) {
+    this.getUrl();
+    this.filterCategories();
+  }
   ngOnInit() {
-    this.dataService.getData().subscribe(
-      result => {
-        this.dataAplication = result;
-      }
-    );
+    this.getDataAplication();
+  }
+
+  filterCategories() {
+    if (this.getUrl() === '/gamesPremium') {
+      this.games = true;
+    } else if (this.getUrl() === '/moviesPremium') {
+      this.movies = true;
+    }
+  }
+  /**
+   * Function to get Url for side-navbar
+   */
+  getUrl() {
+    return this.router.url;
+  }
+
+  /**
+   * Function to get all data information
+   */
+  getDataAplication() {
+    const self = this;
+
+    self.dataService.getData()
+      .subscribe(
+        result => {
+          self.dataAplication = result;
+        }
+      );
   }
 }
